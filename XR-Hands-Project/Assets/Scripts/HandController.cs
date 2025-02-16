93eac256c5257f32f35dfb5c5d63e6bd2fdb542f
Hiski Huhtanen <hhuhtane22@student.oulu.fi>
2025-02-16 19:54:35 +0200

    Wasting time on the hands!
    -This is frustrating!
    -Found a scene to help, will continue with that!

---
+++ XR-Hands-Project/Assets/Scripts/HandController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+[RequireComponent(typeof(ActionBasedController))]
+public class HandController : MonoBehaviour
+{
+    ActionBasedController controller;
+    Hand hand;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
